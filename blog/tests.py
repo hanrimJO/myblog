041d24ef4a739cbbc37753c912ae6a060a098f9e
hanrimJO <whgksfla@gmail.com>
2020-05-19 15:53:10 +0900

    blog: Add post_detail test code

--- blog/tests.py
+++ blog/tests.py
@@ -56,9 +56,12 @@ class TestView(TestCase):
         # 불러온 내용은 맞는가?
         soup = BeautifulSoup(response.content, 'html.parser')
         body = soup.body
-
         self.assertNotIn('아직 게시물이 없습니다', body.text)
         self.assertIn(post_000.title, body.text)
+
+        # readmore 클릭시
+        post_000_read_more_btn = body.find('a', id=f'read-more-post-{post_000.pk}')
+        self.assertEqual(post_000_read_more_btn['href'], post_000.get_absolute_url())
 
     def test_post_detail(self):
         post_000 = create_post(
@@ -79,4 +82,10 @@ class TestView(TestCase):
         self.assertEqual(title.text, f'{post_000.title} - Blog')
 
         self.check_navbar(soup)
+
+        body = soup.body
+        main_div = body.find('div', id='main_div')
+        self.assertIn(post_000.title, main_div.text)
+        self.assertIn(post_000.author.username, main_div.text)
+        self.assertIn(post_000.content, main_div.text)
 
