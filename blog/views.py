f43410be2c9ed64e4f6d6566b0a88945a10407c1
hanrimJO <whgksfla@gmail.com>
2020-05-28 20:01:34 +0900

    blog: Fix only log in user can comment

--- blog/views.py
+++ blog/views.py
@@ -13,7 +13,6 @@ class PostList(ListView):
     model = Post
     paginate_by = 5
 
-
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(PostList, self).get_context_data(**kwargs)
         context['category_list'] = Category.objects.all()
